45ba2f704005995cef16055846e40f55b241f2ef
kapuniko <kapuniko@gmail.com>
2025-04-09 03:00:43 +0500

    fix style

--- resources/views/components/calendar/event.blade.php
+++ resources/views/components/calendar/event.blade.php
@@ -1,14 +1,24 @@
 @props(['event', 'is_multiday' => false])
 
-<div class="emoji @if($is_multiday) multiday-event @endif">
-    {{ $event->emoji }}
-    {{ \Carbon\Carbon::parse($event->event_time)->format('H:i') }}
-    @if($event->amount !== null)
-        {{ $event->amount >= 0 ? '💰' : '💸' }}{{ abs($event->amount) }}
-    @endif
-    @if($is_multiday)
+@if($is_multiday)
+    <div class="emoji multiday-event">
+        {{ $event->emoji }}
+        @if($event->amount !== null)
+            {{ $event->amount >= 0 ? '💰' : '💸' }}{{ abs($event->amount) }}
+        @endif
         <span class="event-duration">
             {{ $event->name }}
         </span>
-    @endif
-</div>
+    </div>
+@else
+    <div class="emoji">
+        {{ $event->emoji }}
+        {{ \Carbon\Carbon::parse($event->event_time)->format('H:i') }}
+        @if($event->amount !== null)
+            {{ $event->amount >= 0 ? '💰' : '💸' }}{{ abs($event->amount) }}
+        @endif
+    </div>
+@endif
+
+
+
