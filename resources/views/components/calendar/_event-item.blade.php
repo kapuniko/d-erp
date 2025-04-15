93108ce7cb02cd0ec3689d1a31c5d581fbe331a9
kapuniko <kapuniko@gmail.com>
2025-04-15 21:44:52 +0500

    style-fix

--- resources/views/components/calendar/_event-item.blade.php
+++ resources/views/components/calendar/_event-item.blade.php
@@ -41,7 +41,7 @@
             x-show="open"
             @click.away="open = false"
             x-transition
-            class="absolute right-0 mt-1 z-50 bg-white border border-gray-200 rounded shadow-md p-1 flex gap-1"
+            class="absolute right-0 mt-1 z-50 bg-white border border-gray-200 rounded shadow-md p-2 flex gap-2"
         >
             <!-- Кнопка скрытия -->
             <button
@@ -57,8 +57,8 @@
                          stroke="currentColor" class="size-4 transition duration-300">
                         <path stroke-linecap="round" stroke-linejoin="round"
                               d="M2.25 12C3.5 7.5 7.5 4.5 12 4.5s8.5 3
-                        9.75 7.5c-1.25 4.5-5.25 7.5-9.75
-                        7.5s-8.5-3-9.75-7.5z" />
+                                    9.75 7.5c-1.25 4.5-5.25 7.5-9.75
+                                    7.5s-8.5-3-9.75-7.5z" />
                         <path stroke-linecap="round" stroke-linejoin="round"
                               d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
@@ -69,12 +69,12 @@
                          stroke="currentColor" class="size-4 transition duration-300">
                         <path stroke-linecap="round" stroke-linejoin="round"
                               d="M3.98 8.223A10.477 10.477 0 0 0 1.934
-            12C3.226 16.338 7.244 19.5 12 19.5c.993 0
-            1.953-.138 2.863-.395M6.228 6.228A10.451
-            10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065
-            7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228
-            3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0
-            0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                12C3.226 16.338 7.244 19.5 12 19.5c.993 0
+                                1.953-.138 2.863-.395M6.228 6.228A10.451
+                                10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065
+                                7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228
+                                3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0
+                                0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                     </svg>
                 </template>
             </button>
