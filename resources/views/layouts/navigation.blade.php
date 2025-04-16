cb83032105a8ac5e31be8ba638e794eb45d9cd4a
kapuniko <kapuniko@gmail.com>
2025-04-16 16:52:10 +0500

    Выводим календарь на стартовую

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,9 +18,11 @@
                 </div>
             </div>
 
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
+                    @if(Auth::user())
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
@@ -49,8 +51,30 @@
                             </x-dropdown-link>
                         </form>
                     </x-slot>
+                    @else
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <div>Войдите, или зарегистрируйтесь, чтобы добавлять свои события</div>
+
+                                <div class="ms-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+                        <x-slot name="content">
+                            <x-dropdown-link href="/login">
+                                Вход
+                            </x-dropdown-link>
+                            <x-dropdown-link href="/register">
+                                Регистрация
+                            </x-dropdown-link>
+                        </x-slot>
+                    @endif
                 </x-dropdown>
             </div>
+
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -72,6 +96,7 @@
             </x-responsive-nav-link>
         </div>
 
+        @if(Auth::user())
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
@@ -96,5 +121,13 @@
                 </form>
             </div>
         </div>
+            @else
+                <x-responsive-nav-link href="/login">
+                    Вход
+                </x-responsive-nav-link>
+                <x-responsive-nav-link href="/register">
+                    Регистрация
+                </x-responsive-nav-link>
+            @endif
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,68 +1,85 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--<!DOCTYPE html>--}}
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+{{--    <head>--}}
+{{--        <meta charset="utf-8">--}}
+{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
 
-        <title>D-ERP</title>
+{{--        <title>D-ERP</title>--}}
 
 
-        <!-- Styles -->
-        <style>
-            html,
-            body{
-                background: black;
-                margin: 0;
-                padding: 0;
-            }
+{{--        <!-- Styles -->--}}
+{{--        <style>--}}
+{{--            html,--}}
+{{--            body{--}}
+{{--                background: black;--}}
+{{--                margin: 0;--}}
+{{--                padding: 0;--}}
+{{--            }--}}
 
-            body{
-                display:flex;
-                flex-direction: column;
-                align-items:center;
-                justify-content: center;
-                height:auto;
-                min-height:100vh;
-                animation: rotate 4s infinite ease-in-out;
-            }
+{{--            body{--}}
+{{--                display:flex;--}}
+{{--                flex-direction: column;--}}
+{{--                align-items:center;--}}
+{{--                justify-content: center;--}}
+{{--                height:auto;--}}
+{{--                min-height:100vh;--}}
+{{--                animation: rotate 4s infinite ease-in-out;--}}
+{{--            }--}}
 
-            .logo{
-                width:300px;
-                margin: 0;
-                padding: 0;
-                filter: drop-shadow(0px 0px 5px blue);
-                animation: float 6s infinite ease-in-out;
-                perspective: 1000px;
-            }
+{{--            .logo{--}}
+{{--                width:300px;--}}
+{{--                margin: 0;--}}
+{{--                padding: 0;--}}
+{{--                filter: drop-shadow(0px 0px 5px blue);--}}
+{{--                animation: float 6s infinite ease-in-out;--}}
+{{--                perspective: 1000px;--}}
+{{--            }--}}
 
-            @keyframes float{
-                0% {
-                    transform:  translateY(0px) rotateX(5deg);
-                }
-                50%{
-                    transform:  translateY(10px) rotateX(-5deg);
-                }
-                100% {
-                    transform:  translateY(0px) rotateX(5deg);
-                }
-            }
+{{--            @keyframes float{--}}
+{{--                0% {--}}
+{{--                    transform:  translateY(0px) rotateX(5deg);--}}
+{{--                }--}}
+{{--                50%{--}}
+{{--                    transform:  translateY(10px) rotateX(-5deg);--}}
+{{--                }--}}
+{{--                100% {--}}
+{{--                    transform:  translateY(0px) rotateX(5deg);--}}
+{{--                }--}}
+{{--            }--}}
 
-            @keyframes rotate{
-                0% {
-                    transform:  rotateY(5deg);
-                }
-                50%{
-                    transform:  rotateX(-5deg);
-                }
-                100% {
-                    transform:  rotateY(5deg);
-                }
-            }
-        </style>
-    </head>
-    <body>
+{{--            @keyframes rotate{--}}
+{{--                0% {--}}
+{{--                    transform:  rotateY(5deg);--}}
+{{--                }--}}
+{{--                50%{--}}
+{{--                    transform:  rotateX(-5deg);--}}
+{{--                }--}}
+{{--                100% {--}}
+{{--                    transform:  rotateY(5deg);--}}
+{{--                }--}}
+{{--            }--}}
+{{--        </style>--}}
+{{--    </head>--}}
+{{--    <body>--}}
 
-        <img class="logo" src="{{ asset('images/d-erp.webp') }}" alt="D-ERP Logo">
+{{--        <img class="logo" src="{{ asset('images/d-erp.webp') }}" alt="D-ERP Logo">--}}
 
-    </body>
-</html>
+{{--    </body>--}}
+{{--</html>--}}
+
+@php use Carbon\Carbon; @endphp
+@push('calendar-assets')
+    @vite(['resources/css/calendar.css', 'resources/js/calendar.js'])
+@endpush
+
+<x-app-layout>
+
+    @php
+        $currentYear = Carbon::now()->year; // Текущий год
+        $currentMonth = Carbon::now()->month; // Текущий месяц
+    @endphp
+
+    @livewire('calendar-month', ['year' => $currentYear, 'month' => $currentMonth])
+
+</x-app-layout>
+
