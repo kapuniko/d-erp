6705d2b83ef9c186f12530127f4c0c837d0b2d9b
kapuniko <kapuniko@gmail.com>
2025-05-11 22:26:49 +0500

    style-fix

--- resources/views/livewire/add-reminder-button.blade.php
+++ resources/views/livewire/add-reminder-button.blade.php
@@ -1,19 +1,33 @@
 @php
-    $color = match($this->status) {
-        'pending' => 'text-green-500',
-        'sent' => 'text-yellow-500',
-        default => 'text-gray-400',
+
+    $reminderIcon = match($this->status) {
+        'pending' => [
+            'text' => 'Напоминалка создана',
+            'color' => 'text-green-500',
+            'path' => 'M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5'
+            ],
+        'sent' => [
+            'text' => 'Напоминалка отправлена',
+            'color' => 'text-green-500',
+            'path' => 'M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+            ],
+        default => [
+            'text' => 'Добавить напоминалку',
+            'color' => 'text-gray-400',
+            'path' => 'M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5'
+            ],
     };
 @endphp
 
-<button wire:click="toggleReminder" class="@if($this->status === 'pending') opacity-100 @else opacity-0 @endif addReminder" title="Управление напоминанием">
+<button wire:click="toggleReminder" class="@if($this->status === 'pending') opacity-100 @else opacity-0 @endif addReminder" title="{{ $reminderIcon['text'] }}">
     <svg xmlns="http://www.w3.org/2000/svg"
          fill="none"
          viewBox="0 0 24 24"
          stroke-width="1.5"
          stroke="currentColor"
-         class="size-4 {{ $color }}">
+         class="size-4 {{ $reminderIcon['color'] }}">
         <path stroke-linecap="round" stroke-linejoin="round"
-              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+              d="{{ $reminderIcon['path'] }}" />
     </svg>
 </button>
+
